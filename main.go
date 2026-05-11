02fc50e72729212a98767addc9abbefec7ac202e
michaelbag8 <michaelbag8@gmail.com>
2026-05-11 21:48:05 +0100

    add: flag

--- main.go
+++ main.go
@@ -3,29 +3,52 @@ package main
 import (
 	"fmt"
 	"os"
+	"flag"
 )
 
 func main() {
-	if len(os.Args) < 2 || len(os.Args) > 3 {
-		fmt.Fprintf(os.Stderr, "Usage: go run . text banner")
+
+	output := flag.String("output", "", "file to write output to")
+
+	flag.Parse()
+
+	args := flag.Args()
+
+	if len(args) < 1{
+		fmt.Fprintf(os.Stderr, "usage go run [--output=file.txt] <string> [banner]")
 		return
 	}
+	// if len(os.Args) < 2 || len(os.Args) > 3 {
+	// 	fmt.Fprintf(os.Stderr, "Usage: go run . text banner")
+	// 	return
+	// }
 
 	fontFile := "standard.txt"
+	input := args[0]
 
-	if len(os.Args) == 3 {
-		fontFile = os.Args[2] + ".txt"
+	if len(args) >=2{
+		fontFile = args[1] 
 	}
 
-	input := os.Args[1]
+	
 
-	data, err := LoadBanner(fontFile)
+	data, err := LoadBanner(fontFile + ".txt")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error ")
 		return
 	}
 
 	content := Generate(input, data)
-	fmt.Print(content)
+	
+	if *output != ""{
+		err := os.WriteFile(*output, []byte(content), 0644)
+		if err != nil {
+		fmt.Fprintf(os.Stderr, "error : writing file %v\n", err)
+		return
+	}
+	fmt.Printf("Output written to %s\n",*output)
+	}else{
+		fmt.Print(content)
+	}
 
 }
